2bafac24f7f6420991a20f1681e268370a6d5588
Arthur Danielson <arthurdanielson7@gmail.com>
2025-07-18 10:19:17 -0400

    feat: seção 1_Introdução

---
+++ 1_Introdução/hello-world/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h2>Emmet funcionou</h2>
+      <div className="container">
+        <p>meu elemento</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
