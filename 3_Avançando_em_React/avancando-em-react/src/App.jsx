d322bd6abf5c57498943d350a9b5492cb0e4a446
Arthur Danielson <arthurdanielson7@gmail.com>
2025-07-21 10:03:56 -0400

    Revert "feat: 20/-7/2025 #2"
    This reverts commit 43cb4500ea84957f17cf01877dd932fd4bc08848.

--- 3_Avançando_em_React/avancando-em-react/src/App.jsx
+++ 3_Avançando_em_React/avancando-em-react/src/App.jsx
@@ -1,17 +1,11 @@
-import React, { useState } from "react";
 import "./App.css";
 
 import City from "./assets/city.jpg";
 import ConditionalRender from "./components/ConditionalRender";
 import ListRender from "./components/ListRender";
 import ManageData from "./components/ManageData";
-import ShowUserName from "./components/ShowUserName";
-import CarDetails from "./components/CarDetails";
 
 function App() {
-  const name = "Vitor";
-  const [userName] = useState("Maria");
-
   return (
     <>
       <h1>Avançando em React</h1>
@@ -26,10 +20,6 @@ function App() {
       <ManageData />
       <ListRender />
       <ConditionalRender />
-      {/* props */}
-      <ShowUserName name={userName} />
-      {/* destructuring */}
-      <CarDetails brand="VW" km={100000} color="Azul" />
     </>
   );
 }
